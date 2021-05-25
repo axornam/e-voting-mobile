2097cf846c4cd97cc62abdc6e05de132d0780642
Richard Axornam <axornam.richard@gmail.com>
2021-05-25 01:04:58 +0000

    Add login and register fragments

---
+++ app/src/main/java/com/progress/e_voting/AuthActivity.java
@@ -0,0 +1,15 @@
+package com.progress.e_voting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuthActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+    }
+}
