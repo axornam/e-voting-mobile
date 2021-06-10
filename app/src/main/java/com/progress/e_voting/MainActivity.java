b472d2e038a30bebe36d6ed7abee1bc0df52d28e
axornam <axornam.richard@gmail.com>
2021-06-10 17:41:35 +0000

    Add logout resource file and functionality

--- app/src/main/java/com/progress/e_voting/MainActivity.java
+++ app/src/main/java/com/progress/e_voting/MainActivity.java
@@ -1,12 +1,20 @@
 package com.progress.e_voting;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -23,4 +31,36 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navView, navController);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.app_bar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id) {
+            case R.id.menu_logout:
+                // todo logout
+                FirebaseAuth.getInstance().signOut();
+
+                // todo redirect to login page
+                if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+                    Intent intent = new Intent(getApplicationContext(), AuthActivity.class);
+                    startActivity(intent);
+                    finish();
+                    Toast.makeText(this, "You've Been Logged Out", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                return false;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
